d07c5fcb8a6afd1ac06713dbbae7a9ed12411726
Mees Brinkhuis <m.brinkhuis@student.tudelft.nl>
2016-05-29 18:46:12 +0200

    Now has multiple boards, 2 by default

--- database.sql
+++ database.sql
@@ -4,15 +4,39 @@ DROP SCHEMA IF EXISTS `DevBoard` ;
 CREATE SCHEMA IF NOT EXISTS `DevBoard` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 USE `DevBoard` ;
 
--- create table
+-- Create table for posts
 DROP TABLE IF EXISTS `DevBoard`.`post` ;
 
 CREATE TABLE IF NOT EXISTS `DevBoard`.`post` (
-  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `Id` INT(11) NOT NULL,
   `Name` TEXT NULL,
   `Subject` TEXT NULL,
 	`Comment` TEXT NULL,
   `CreationDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   `HasImage` BOOLEAN NOT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+-- Create table for threads
+DROP TABLE IF EXISTS `DevBoard`.`thread` ;
+
+CREATE TABLE IF NOT EXISTS `DevBoard`.`thread` (
+  `Id` INT(11) NOT NULL,
+  `Name` TEXT NULL,
+  `Subject` TEXT NULL,
+	`Comment` TEXT NULL,
+  `CreationDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Board` TEXT NOT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+-- Links posts and thread toghether
+DROP TABLE IF EXISTS `DevBoard`.`link` ;
+
+CREATE TABLE IF NOT EXISTS `DevBoard`.`link` (
+  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `ThreadId` INT(11) NOT NULL,
+  `PostId` INT(11) NOT NULL,
   PRIMARY KEY (`Id`)
 )
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var http = require("http");
 var multer = require("multer");
 var express = require("express");
 var mysql = require("mysql");
+var ejs = require("ejs");
 
-var upload = multer({ dest: 'public/uploads/' })
+var upload = multer({ dest: 'tmp/' })
 var app = express();
+
+//set views
+app.set('views', __dirname + '/static/views');
+app.set('view engine', 'ejs');
 
 //create server
 http.createServer(app).listen(8080);
@@ -21,19 +26,63 @@ var connection = mysql.createConnection({
 
 //connect to database
 connection.connect(function(err) {
-  console.log("Connected to database");
+  if(!err){
+    console.log("Connected to database");
+  } else {
+    console.log("Could not connect to database");
+    return;
+  }
 });
 
-//uploading files
+//render board
+//currently only /b/ and /g/ are there
+app.get("/:type", function(req, res){
+  var type = req.params.type;
+  var title;
+  switch(type){
+    case "b":
+      title = "/b/ - Random";
+      break;
+    case "g":
+      title = "/g/ - Technology";
+      break;
+    default:
+      res.render("default", { type: type});
+      return;
+  }
+  res.render("board", {
+    board: type,
+    title: title
+  });
+});
+
+//render thread
+app.get("/thread/:type", function(req, res){
+  var type = req.params.type;
+  var title = "empty";
+  connection.query("SELECT Id FROM thread WHERE Id=\"" + type + "\"", function(err, result){
+    if(result[0] != null){
+      res.render("thread", {
+        thread: type,
+        title: title
+      });
+    } else {
+      res.render("default", { type: type});
+    }
+  });
+});
+
+//posting in threads
 app.post("/upload", upload.single("image"), function(req, res){
   console.log("upload request");
 
   //get current highest id
-  connection.query("SELECT MAX(Id) AS Id FROM post", function(err, result) {
+  connection.query("select MAX(GREATEST(post.Id, thread.Id)) AS Id from thread, post", function(err, result) {
 
     //get id
     var id = result[0].Id;
     if(id == null ) id = 0;
+    id++;
 
     //make it so that the database stays clean
     var name = "\"" + req.body.name + "\"";
@@ -43,25 +92,50 @@ app.post("/upload", upload.single("image"), function(req, res){
     if(subject == "\"\"") subject = "NULL";
     if(comment == "\"\"") comment = "NULL";
 
-    //if there's an image upload
-    if(req.file){
-      //get file extention
-      var mime = req.file.mimetype.split("/")[1];
+    if(req.body.type == "post"){
+      //POSTS
+      //if there's an image upload
+      console.log("post");
+      if(req.file){
+        //get file extention
+        var mime = req.file.mimetype.split("/")[1];
 
-      var query = "INSERT INTO post (Name, Subject, Comment, HasImage) "
-                + "VALUES (" + name + "," + subject
-                + "," + comment + ", 1)";
+        var query = "INSERT INTO post (Id, Name, Subject, Comment, HasImage) "
+                  + "VALUES (" + id + "," + name + "," + subject
+                  + "," + comment + ", 1)";
+        connection.query(query);
+        fs.mkdirSync("public/uploads/");
+        fs.rename(req.file.path, "public/uploads/"+id+"."+mime);
+      }
+
+      //if there is no image upload
+      else {
+        var query = "INSERT INTO post (Id, Name, Subject, Comment, HasImage) "
+                  + "VALUES (" + id + "," + name + "," + subject
+                  + "," + comment + ", 0)";
+        connection.query(query);
+      }
+
+      //link to thread
+      var query = "INSERT INTO link (ThreadId, PostId) VALUES (" + req.body.belong
+                 + "," + id + ")";
       connection.query(query);
-      fs.rename(req.file.path, "public/uploads/"+(id+1)+"."+mime);
+
+      //update thread update time
+      //some stuff about timezones
+      var time = new Date().toISOString().slice(0, 19).replace('T', ' ');
+      var query = "UPDATE thread SET UpdatedTime=\"" + time + "\" WHERE Id=\""
+                  + req.body.belong + "\"";
+      connection.query(query);
     }
 
-    //if there is no image upload
-    else {
-      var query = "INSERT INTO post (Name, Subject, Comment, HasImage) "
-                + "VALUES (" + name + "," + subject
-                + "," + comment + ", 0)";
+    //THREADS
+    else{
+      console.log("thread");
+      var query = "INSERT INTO thread (Id, Name, Subject, Comment, Board) "
+                + "VALUES (" + id + "," + name + "," + subject
+                + "," + comment + ",\"" + req.body.belong + "\")";
       connection.query(query);
     }
-
   });
 });
