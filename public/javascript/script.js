21ccd79fdc1027297a7911e4bee1e7f1c427cbe4
Mees Brinkhuis <m.brinkhuis@student.tudelft.nl>
2016-06-02 19:23:32 +0200

    Saving bandwidth (i should commit less)

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -77,7 +77,7 @@ function getThreads(type, boardId, threadId){
       var mime = result[i].Image.split(".")[1];
 
       if(mime == "webm" || mime == "mp4" || mime == "ogg") {
-        var image = "<a href=\"/uploads/" + result[i].Image +"\"><video>"
+        var image = "<a href=\"/uploads/" + result[i].Image +"\"><video preload=\"metadata\">"
                   + "<source src=\"/uploads/" + result[i].Image +"\" type=\"video/"+mime+"\"/>"
                   + "</video></a>";
       } else if(mime == "jpeg" || mime == "gif" ||mime == "png" ||mime == "svg" || mime == "bmp"){
@@ -139,7 +139,7 @@ function getPosts(thread, amount){
       if(posts[i].Comment == null) posts[i].Comment = "";
 
       if(mime == "webm" || mime == "mp4" || mime == "ogg") {
-        var image = "<a href=\"/uploads/" + posts[i].Image +"\"><video>"
+        var image = "<a href=\"/uploads/" + posts[i].Image +"\"><video preload=\"metadata\">"
                   + "<source src=\"/uploads/" + posts[i].Image +"\" type=\"video/"+mime+"\"/>"
                   + "</video></a>";
       } else if(mime == "jpeg" || mime == "gif" ||mime == "png" ||mime == "svg" || mime == "bmp"){
