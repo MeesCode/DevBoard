2e1c3ce56e7d12c22126f4449df64e166e9f5c70
Mees Brinkhuis <m.brinkhuis@student.tudelft.nl>
2016-11-04 19:39:07 +0100

    Changed database

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
--- running this will empty your old database
+-- running this will empty your old database and create a new one
 
 DROP SCHEMA IF EXISTS `DevBoard` ;
 CREATE SCHEMA IF NOT EXISTS `DevBoard` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
@@ -8,12 +8,12 @@ USE `DevBoard` ;
 DROP TABLE IF EXISTS `DevBoard`.`post` ;
 
 CREATE TABLE IF NOT EXISTS `DevBoard`.`post` (
-  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `Id` INT NOT NULL AUTO_INCREMENT,
   `Name` TEXT NULL,
   `Subject` TEXT NULL,
   `Comment` TEXT NULL,
-  `CreationDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `Thread` INT(11) NOT NULL,
+  `CreationTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `Thread` INT NOT NULL,
   `Image` TEXT NULL,
   `IsThread` BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY (`Id`)
@@ -23,14 +23,28 @@ CREATE TABLE IF NOT EXISTS `DevBoard`.`post` (
 DROP TABLE IF EXISTS `DevBoard`.`thread` ;
 
 CREATE TABLE IF NOT EXISTS `DevBoard`.`thread` (
-  `Id` INT(11) NOT NULL,
+  `Id` INT NOT NULL,
   `Name` TEXT NULL,
   `Subject` TEXT NULL,
   `Comment` TEXT NULL,
-  `CreationDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `CreationTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `UpdatedTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `Board` TEXT NOT NULL,
   `Image` TEXT NOT NULL,
+  `Pinned` BOOLEAN DEFAULT FALSE,
+  `Closed` BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (`Id`)
+);
+
+-- Create table for images
+DROP TABLE IF EXISTS `DevBoard`.`image` ;
+
+CREATE TABLE IF NOT EXISTS `DevBoard`.`image` (
+  `Id` INT NOT NULL,
+  `Name` TEXT NULL,
+  `ImageId` INT NOT NULL,
+  `Extention` TEXT NOT NULL,
+  `Spoiler` BOOLEAN DEFAULT FALSE,
   PRIMARY KEY (`Id`)
 );
 
@@ -40,6 +54,7 @@ DROP TABLE IF EXISTS `DevBoard`.`board` ;
 CREATE TABLE IF NOT EXISTS `DevBoard`.`board` (
   `Id` VARCHAR(5) NOT NULL,
   `Title` TEXT NOT NULL,
+  `Nsfw` BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY (`Id`)
 );
 
@@ -47,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `DevBoard`.`board` (
 DROP TABLE IF EXISTS `Devboard`.`announcements` ;
 
 CREATE TABLE IF NOT EXISTS `DevBoard`.`announcements` (
-  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `Id` INT NOT NULL AUTO_INCREMENT,
   `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Comment` text NOT NULL,
   PRIMARY KEY (`Id`)
